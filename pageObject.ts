2776beb2022a519b7d3ae52e052ea2f7bc689853
mray1997 <95275915+mray1997@users.noreply.github.com>
2022-03-11 17:27:54 -0800

    Update pageObject.ts

--- pageObject.ts
+++ pageObject.ts
@@ -1,1 +1,18 @@
-//example text
+import {By} from "selenium-webdriver"
+import {basePage} from "./basePage"
+
+export class Petco extends basePage {
+    searchBar: By = By.xpath('//input[@id="header-search"]');
+    results: By = By.xpath('//h1[@class="EllipsisTextContainer-wz2pam-1 ihPcpc"]')
+    addToCart: By = By.xpath('//button[@class="AddToCartButton-sc-1qdddxl-0 gzVKkV"]')
+    viewCart: By = By.xpath('//a[text()= "VIEW CART & CHECKOUT"]')
+    constructor() {
+        super({url: "https://www.petco.com/shop/en/petcostore"});
+    }
+    async search(searchTerm: string) {
+        return this.setInput(this.searchBar, `${searchTerm}\n`)
+    }
+    async getResults() {
+        return this.getText(this.results)
+    }
+}
